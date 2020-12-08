e6101977a6b6b372c772a30cbdf1a0d7201b4c79
sodapopcurtis <trevor@bielarski.com>
2020-12-07 19:15:37 -0500

    New repo name

--- src/components/UpdateAlbumComponent.jsx
+++ src/components/UpdateAlbumComponent.jsx
@@ -42,7 +42,7 @@ class UpdateAlbumComponent extends Component {
         console.log('album => ' + JSON.stringify(album));
         console.log('id => ' + JSON.stringify(this.state.id));
         AlbumService.updateAlbum(album, this.state.id).then( res => {
-            this.props.history.push('/crud-frontend-app/albums');
+            this.props.history.push('/crud-frontend/albums');
         });
     }
     
@@ -63,7 +63,7 @@ class UpdateAlbumComponent extends Component {
     }
 
     cancel(){
-        this.props.history.push('/crud-frontend-app/albums');
+        this.props.history.push('/crud-frontend/albums');
     }
 
     render() {
